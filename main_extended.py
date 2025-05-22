0865a2169ce3790fb15afb7cb461863370d26156
Philip Münster-Hansen <phillermh@gmail.com>
2025-05-22 23:37:56 +0200

    Finish computing extended model features

--- main_extended.py
+++ main_extended.py
@@ -9,29 +9,28 @@ mask_path = os.getenv("MASK_DATA_URL_LOCAL")
 
 if __name__ == "__main__":
     loader = ImageDataLoader(images_path, mask_path, hairless = True)
-    df = pd.DataFrame(columns=["img_id", "Label"])
+
+    df = pd.DataFrame(columns=["img_id", "Melanoma"])
     metadata = pd.read_csv("data/metadata.csv")
     df["img_id"] = metadata["img_id"]
-    df["Label"] = metadata["diagnostic"]
+    df["Melanoma"] = metadata["diagnostic"]
 
-    cancer = ["BCC", "SCC", "MEL"]
-    df["Label"] = df["Label"].isin(cancer).astype(int)
-
-    print("Please input feature version suffix:")
-    version_suffix = input()
+    cancer = ["MEL"]
+    df["Melanoma"] = df["Melanoma"].isin(cancer).astype(int)
 
     rows = []
-    i = 0
-    A = "feature_a_" + version_suffix
-    B = "feature_b_" + version_suffix
-    C = "feature_c_" + version_suffix
 
+    A = "feature_a"
+    B = "feature_b"
+    C = "feature_c"
+
+    i = 1
     for filename, assymetry, _border, color in loader:
         img_id = os.path.basename(filename)
+        print(f"Now loading: {img_id}")
         rows.append({"img_id": img_id, A: assymetry, B: _border, C: color})
+        print(f"{i} done {2297-i} to go")
         i += 1
-        if i == 4:
-            break
     
     df_features = pd.DataFrame(rows)
     df_features["Z_" + A] = (df_features[A] - df_features[A].mean()) / df_features[A].std()
