117e81bd44d1593941de07ea6e04e7281fd2749d
jan-cardell <janpetercardell@outlook.de>
2025-05-22 19:47:43 +0200

    Baseline Train/Test Split

--- util/data-split.py
+++ util/data-split.py
@@ -1,19 +1,17 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def data_split(input_csv, ):
-    df = pd.read_csv(input_csv, )
+def data_split(input_csv):
+    df = pd.read_csv(input_csv)
 
     train_df, test_df = train_test_split(
     df,
     test_size=0.2,
-    stratify=df['diagnostic'],
+    stratify=df['Melanoma'],
     random_state=42
     )
 
-    cancerous_types = ['BCC', 'SCC', 'MEL']
-    test_df['cancerous'] = test_df['diagnostic'].isin(cancerous_types).astype(int)
-    train_df['cancerous'] = train_df['diagnostic'].isin(cancerous_types).astype(int) 
-    
-    train_df.to_csv("train-model-data.csv", index=False)
-    train_df.to_csv("test-model-data.csv", index=False)
+    train_df.to_csv("../data/train-baseline-data.csv", index=False)
+    test_df.to_csv("../data/test-baseline-data.csv", index=False)
+
+data_split("../data/baseline-data-for-model.csv")
